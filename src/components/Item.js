072f30f3fa9c524c15370f9cd5e7cbd17279a1fb
sarayfg <sarayfernandezgarcia@gmail.com>
2021-08-22 20:07:39 +0200

    remove debug code

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,7 +4,6 @@ import React, {Component} from 'react';
 class Item extends Component {
     render() {
         const { data } = this.props;
-        console.log(data);
         return (
         <div>{data.name}</div>
         );
